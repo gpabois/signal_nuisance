ed1ab8ae84187efaba4d0219043dfc575e9b5863
PABOIS Gaël <gael.pabois@jade.local>
2022-03-18 17:25:10 +0100

    up

--- lib/signal_nuisance_web/views/error_helpers.ex
+++ lib/signal_nuisance_web/views/error_helpers.ex
@@ -10,8 +10,8 @@ defmodule SignalNuisanceWeb.ErrorHelpers do
   """
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, translate_error(error),
-        class: "invalid-feedback",
+      content_tag(:div, translate_error(error),
+        class: "text-danger",
         phx_feedback_for: input_name(form, field)
       )
     end)
